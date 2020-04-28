42d110abec8fea0fd081036cee7109926d5fe4c9
joao front <joao.saousa@tangotech.com.br>
2020-04-28 16:37:13 -0300

    implementando formulario

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  Jumbotron, Button, Form, Col, Spinner, Alert, Modal
+} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import ListarMoedas from './listar-moedas'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Jumbotron>
+        <h1>Algum dia direi não foi fácil mas eu consegui!!</h1>
+        <Alert variant="danger" show={false}>
+          erro obtendo dados de conversão tente novamente
+        </Alert>
+        <Form>
+          <Form.Row>
+
+            <Col sm="3">
+              <Form.Control
+                placeholder="0"
+                value={1}
+                required />
+            </Col>
+            <Col sm="3">
+              <Form.Control
+                as="select"
+              >
+                <ListarMoedas/>
+              </Form.Control>
+            </Col>
+            <Col sm="1" className="text-center" style={{ paddingTop: '5px' }}>
+              <FontAwesomeIcon icon={faAngleDoubleRight} />
+            </Col>
+            <Col sm="3">
+              <Form.Control
+                as="select"
+              >
+                 <ListarMoedas/>
+              </Form.Control>
+            </Col>
+            <Col sm="2">
+              <Button variant="success type=" onSubmit>
+                <Spinner animation="border" size="3" />
+                Converter
+              </Button>
+            </Col>
+          </Form.Row>
+
+        </Form>
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              conversão
+          </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Resultado da conversão aqui...
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">
+              Nova conversão
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+      </Jumbotron>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('deve renderizar o componente sem erros', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
